c346393761ce08f1b9afb229ab48a391cbc31804
Sascha Müllner <sascha.muellner@gmail.com>
2021-11-07 15:50:06 +0100

    init swift

---
+++ Sources/Jar/Jar.swift
@@ -0,0 +1,6 @@
+public struct Jar {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/JarTests/JarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Jar
+
+final class JarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Jar().text, "Hello, World!")
+    }
+}
