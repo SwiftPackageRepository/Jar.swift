df0fd4fedbe5861e61c0b9031a8a2f878813ae8b
Sascha Müllner <sascha.muellner@gmail.com>
2021-11-09 09:04:55 +0100

    added hello world jar

--- Examples/HelloWorldJar/Sources/HelloWorldJar/HelloWorld.swift
+++ Examples/HelloWorldJar/Sources/HelloWorldJar/HelloWorld.swift
@@ -3,11 +3,16 @@ import Jar
 
 struct HelloWorld {
     static func run() {
-        print("== Installed JVMs")
-        let url = URL(string: "https://repo1.maven.org/maven2/org/vafer/helloworld/1.0/helloworld-1.0.jar")!
+        print("== Running helloworld.jar")
+        let url = URL(string: "https://github.com/slaminatl/Helloworld/raw/master/out/artifacts/HelloWorld_jar/HelloWorld.jar")!
         let jar = Jar(origin: url, filename: "helloworld.jar")
         let java = Java()
-        java.r
+        let (result, error) = java.run(jar: jar, args: [])
+        if let result = result {
+            print(result)
+        } else if let error = error {
+            print(error.localizedDescription)
+        }
     }
 }
 
